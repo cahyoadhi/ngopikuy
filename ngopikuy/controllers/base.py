b645fccb5f1d16e31c9cbed6fd827bad69c2ede5
unknown <cahyoadhianto2507@gmail.com>
2022-09-21 04:51:51 +0700

    main

---
+++ ngopikuy/controllers/base.py
@@ -0,0 +1,20 @@
+from ngopikuy.models import Product
+from django.shortcuts import render
+from .employee import *
+from .customer import *
+from .blogs import *
+from .authentication import *
+
+def HomePage(request):
+    favmenu = Product.objects.filter(tags__name__in=["FAV"])
+    context = {'favs':favmenu}
+    return render (request, 'index.html',context)
+
+def ContactPage(request):
+    context ={}
+    return render (request, 'page/contact.html',context)
+
+def AboutPage(request):
+    context ={}
+    return render (request, 'page/about.html',context)   
+
--- ngopikuy/urls.py
+++ ngopikuy/urls.py
@@ -1,28 +1,28 @@
 from django.urls import path
-from .controllers import views, authentication, employee, customer, blogs
+from . import views
 
 # url routing
 urlpatterns = [
     path('',views.HomePage,name="homepage"),
     path('about/',views.AboutPage,name="about"),
     path('contact/',views.ContactPage,name="contact"),
-    path('login/',authentication.LoginPage,name="login"),
-    path('logout/',authentication.LogoutUser,name="logout"),
-    path('register/',authentication.RegisterPage,name="register"),  
-    path('dashboard/',employee.DashBoardPage,name="dashboard"),
-    path('orderlist/',employee.OrderListPage,name="orderlist"),
-    path('productlist/',employee.ProductListPage,name="productlist"),
-    path('stream/', employee.PostStreamView.as_view(), name='stream'),
-    path('order/<pk>/', employee.OrderDetail.as_view(), name='order_detail'),
-    path('delete/product/<pk>/', employee.DeleteProduct, name='delete_product'),
-    path('product/<pk>/', employee.EditProduct, name='edit_product'),
-    path('add_product/', employee.AddProduct, name='add_product'),
-    path('customer/',customer.CustomerPage,name="customer"),
-    path('checkout/',customer.ChekcoutPage,name="checkout"),
-    path('blogs/', blogs.PostList, name='blogs'),
-    path('add_post/', blogs.AddPostView, name='add_post'),
-    path('blogsedit/',blogs.BlogListPage,name="blogsedit"),
-    path('edit/<slug:slug>/', blogs.EditPost, name='edit_post'),
-    path('<slug:slug>/', blogs.PostDetail.as_view(), name='post_detail'),
+    path('login/',views.LoginPage,name="login"),
+    path('logout/',views.LogoutUser,name="logout"),
+    path('register/',views.RegisterPage,name="register"),  
+    path('dashboard/',views.DashBoardPage,name="dashboard"),
+    path('orderlist/',views.OrderListPage,name="orderlist"),
+    path('productlist/',views.ProductListPage,name="productlist"),
+    path('stream/', views.PostStreamView.as_view(), name='stream'),
+    path('order/<pk>/', views.OrderDetail.as_view(), name='order_detail'),
+    path('delete/product/<pk>/', views.DeleteProduct, name='delete_product'),
+    path('product/<pk>/', views.EditProduct, name='edit_product'),
+    path('add_product/', views.AddProduct, name='add_product'),
+    path('customer/',views.CustomerPage,name="customer"),
+    path('checkout/',views.ChekcoutPage,name="checkout"),
+    path('blogs/', views.PostList, name='blogs'),
+    path('add_post/', views.AddPostView, name='add_post'),
+    path('blogsedit/',views.BlogListPage,name="blogsedit"),
+    path('edit/<slug:slug>/', views.EditPost, name='edit_post'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
 
