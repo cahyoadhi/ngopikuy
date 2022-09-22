c851ea6a240bc4ecdfc9568dd5467f467f16cb9c
cahyoadhi <90748704+cahyoadhi@users.noreply.github.com>
2022-09-22 07:41:40 +0700

    change procfile

--- coffeeshop/settings.py
+++ coffeeshop/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rr&j*j(m)!)0$+pvqd-_uec7)!g344ia4b4ql)!ed48r8#@@lt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -122,11 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
